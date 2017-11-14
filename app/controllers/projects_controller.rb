8e744427171847ad6f9aed72e6d80a3a3055ce78
Joao Moura <jg.moura.almeida@gmail.com>
2017-11-13 18:24:06 -0600

    Add sorting to project table

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,7 +5,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.all
+    @projects = Project.joins(:project_criterium).select('projects.*, sum(weight) as total').group(:project_id).order('total DESC')
   end
 
   # GET /projects/1
