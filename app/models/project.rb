732d3ae06fc8b7fc2dcb19dacaba3c67f9fc91f4
Joao Moura <jg.moura.almeida@gmail.com>
2017-11-22 15:47:06 -0600

    Add criterium validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,9 @@ class Project < ApplicationRecord
     has_many :project_criterium
     accepts_nested_attributes_for :project_criterium    
     validates_presence_of :name, :description
+    validate :has_criterium?
+    
+    def has_criterium?
+      errors[:base] << "Debe tener minimo un criterio" if self.project_criterium.blank?
+    end
 end
